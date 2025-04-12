b0a80a0d70e6e68686f92a3e09a9b30ccfd70f95
gui10l1 <guiribeiro2003@gmail.com>
2025-04-12 01:15:00 -0300

    add: login screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,183 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Dimensions, Image, KeyboardAvoidingView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import logo from './assets/Logo.png';
+import Constants from 'expo-constants';
+import { useRef, useState } from 'react';
+import { Feather } from '@expo/vector-icons';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const handleFormSubmit = () => {
+    if (!email || !password) {
+      Alert.alert('Inválido!', 'Informa seu email e senha');
+      return;
+    }
+
+    const data = JSON.stringify({ email, password });
+
+    Alert.alert('Legal!', `Dados enviados:\n\n ${data}`);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.keyboardAvoidingView}>
+      <ScrollView style={styles.container}>
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Bem-vindo</Text>
+            <Text style={styles.headerText}>ao</Text>
+            <Image source={logo} resizeMode="contain" />
+          </View>
+
+          <View style={styles.form}>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Email</Text>
+              <View style={styles.inputWrapper}>
+                <Feather name="mail" size={28} color="#000" />
+
+                <TextInput
+                  placeholder="Login with Outlook"
+                  placeholderTextColor="#fff"
+                  style={styles.input}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onSubmitEditing={() => passwordInputRef.current?.focus()}
+                />
+              </View>
+            </View>
+
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Senha</Text>
+
+              <View style={styles.inputWrapper}>
+                <TouchableOpacity style={styles.togglePasswordButton} onPress={() => setShowPassword(!showPassword)}>
+                  <Feather name={showPassword ? 'eye-off' : 'eye'} size={28} color="#000" />
+                </TouchableOpacity>
+
+                <TextInput
+                  placeholder="Enter your Password"
+                  placeholderTextColor="#fff"
+                  style={styles.input}
+                  onChangeText={setPassword}
+                  ref={passwordInputRef}
+                  secureTextEntry={!showPassword}
+                  autoCapitalize="none"
+                  textContentType="oneTimeCode"
+                  onSubmitEditing={handleFormSubmit}
+                  returnKeyType="done"
+                />
+              </View>
+            </View>
+
+            <TouchableOpacity style={styles.forgotPasswordButton}>
+              <Text style={styles.forgotPasswordButtonText}>Esqueci minha senha</Text>
+            </TouchableOpacity>
+
+            <View style={styles.buttonWrapper}>
+              <TouchableOpacity style={styles.submitButton} onPress={handleFormSubmit}>
+                <Text style={styles.submitButtonText}>Entrar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+    paddingHorizontal: 16,
+    paddingBottom: 8,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 8,
+  },
+  header: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  headerText: {
+    fontSize: 32,
+    fontWeight: '400',
+  },
+  form: {
+    marginTop: 32,
+    gap: 16,
+  },
+  formGroup: {
+    gap: 4,
+  },
+  label: {
+    fontSize: 20,
+    paddingLeft: 8,
+  },
+  inputWrapper: {
+    height: 44,
+    backgroundColor: 'hsla(0, 0%, 80%, 1)',
+    borderRadius: 40,
+    paddingHorizontal: 14,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    paddingLeft: 16,
+    height: '100%',
+    color: '#151515',
+    fontSize: 16,
+  },
+  togglePasswordButton: {
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  forgotPasswordButton: {
+    paddingVertical: 6,
+    paddingLeft: 8,
+  },
+  forgotPasswordButtonText: {
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  submitButton: {
+    height: 62,
+    paddingHorizontal: 74,
+    backgroundColor: 'hsla(0, 71%, 50%, 1)',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderRadius: 40,
+
+    // iOS shadow
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+    // Android shadow
+    elevation: 4,
+  },
+  submitButtonText: {
+    fontSize: 26,
+    color: 'white',
   },
 });
